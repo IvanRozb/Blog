adb1e7b75c54dccbd66ef3dab05aa7a82914de3e
huan_rozb <ivanrozb4225@gmail.com>
2023-01-29 20:36:52 +0200

    set up "Featured Posts" part

---
+++ components/posts/posts-grid.js
@@ -0,0 +1,13 @@
+import classes from './posts-grid.module.css';
+
+export default function PostsGrid(props) {
+	const { posts } = props;
+
+	return (
+		<ul className={classes.grid}>
+			{posts.map((post) => (
+				<PostsItem />
+			))}
+		</ul>
+	);
+}
