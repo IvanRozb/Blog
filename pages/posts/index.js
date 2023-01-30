477835017b82494c2420f78246c5dda24809c153
huan_rozb <ivanrozb4225@gmail.com>
2023-01-30 11:21:51 +0200

    added The Posts Page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,1 +1,40 @@
-export default function AllPostsPage() {}
+import AllPosts from '@/components/posts/all-posts';
+
+const TEST_DATA = [
+	{
+		title: 'Getting Started With Next JS1',
+		date: '2023-01-29',
+		excerpt:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium, libero eu laoreet sagittis, purus risus dignissim tellus, id mollis. ',
+		image: 'getting-started-nextjs.png',
+		slug: 'getting-started-nextjs1',
+	},
+	{
+		title: 'Getting Started With Next JS2',
+		date: '2023-01-29',
+		excerpt:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium, libero eu laoreet sagittis, purus risus dignissim tellus, id mollis. ',
+		image: 'getting-started-nextjs.png',
+		slug: 'getting-started-nextjs2',
+	},
+	{
+		title: 'Getting Started With Next JS3',
+		date: '2023-01-29',
+		excerpt:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium, libero eu laoreet sagittis, purus risus dignissim tellus, id mollis. ',
+		image: 'getting-started-nextjs.png',
+		slug: 'getting-started-nextjs3',
+	},
+	{
+		title: 'Getting Started With Next JS4',
+		date: '2023-01-29',
+		excerpt:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium, libero eu laoreet sagittis, purus risus dignissim tellus, id mollis. ',
+		image: 'getting-started-nextjs.png',
+		slug: 'getting-started-nextjs4',
+	},
+];
+
+export default function AllPostsPage() {
+	return <AllPosts posts={TEST_DATA} />;
+}
