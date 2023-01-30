3e834e14f8b02af8d9a2dcd8cb4768fadbbfb64a
huan_rozb <ivanrozb4225@gmail.com>
2023-01-30 13:00:00 +0200

    added SSR to home and posts pages

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,21 @@ import { Fragment } from 'react';
 
 import { Hero } from '@/components/home-page/hero';
 import FeaturedPosts from '@/components/home-page/featured-posts';
+import { getFeaturedPosts } from '@/lib/posts-util';
 
-const TEST_DATA = [
-	{
-		title: 'Getting Started With Next JS',
-		date: '2023-01-29',
-		excerpt:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium, libero eu laoreet sagittis, purus risus dignissim tellus, id mollis. ',
-		image: 'getting-started-nextjs.png',
-		slug: 'getting-started-nextjs',
-	},
-];
-
-export default function HomePage() {
+export default function HomePage(props) {
 	return (
 		<Fragment>
 			<Hero />
-			<FeaturedPosts posts={TEST_DATA} />
+			<FeaturedPosts posts={props.featuredPosts} />
 		</Fragment>
 	);
+}
+
+export async function getStaticProps() {
+	return {
+		props: {
+			featuredPosts: getFeaturedPosts(),
+		},
+	};
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,40 +1,15 @@
 import AllPosts from '@/components/posts/all-posts';
+import { getAllPosts } from '@/lib/posts-util';
 
-const TEST_DATA = [
-	{
-		title: 'Getting Started With Next JS1',
-		date: '2023-01-29',
-		excerpt:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium, libero eu laoreet sagittis, purus risus dignissim tellus, id mollis. ',
-		image: 'getting-started-nextjs.png',
-		slug: 'getting-started-nextjs',
-	},
-	{
-		title: 'Getting Started With Next JS2',
-		date: '2023-01-29',
-		excerpt:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium, libero eu laoreet sagittis, purus risus dignissim tellus, id mollis. ',
-		image: 'getting-started-nextjs.png',
-		slug: 'getting-started-nextjs2',
-	},
-	{
-		title: 'Getting Started With Next JS3',
-		date: '2023-01-29',
-		excerpt:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium, libero eu laoreet sagittis, purus risus dignissim tellus, id mollis. ',
-		image: 'getting-started-nextjs.png',
-		slug: 'getting-started-nextjs3',
-	},
-	{
-		title: 'Getting Started With Next JS4',
-		date: '2023-01-29',
-		excerpt:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium, libero eu laoreet sagittis, purus risus dignissim tellus, id mollis. ',
-		image: 'getting-started-nextjs.png',
-		slug: 'getting-started-nextjs4',
-	},
-];
+export default function AllPostsPage(props) {
+	return <AllPosts posts={props.posts} />;
+}
 
-export default function AllPostsPage() {
-	return <AllPosts posts={TEST_DATA} />;
+export async function getStaticProps() {
+	return {
+		props: {
+			posts: getAllPosts(),
+		},
+		revalidate: 600,
+	};
 }
