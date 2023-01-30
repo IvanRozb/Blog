9a922ca8c80aa1762052e8a97300655158ac6d0f
huan_rozb <ivanrozb4225@gmail.com>
2023-01-30 13:27:34 +0200

    added postDetail prerender page

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,31 @@
 import PostContent from '@/components/posts/post-detail/post-content';
+import { getPostData, getPostsFiles } from '@/lib/posts-util';
 
-export default function PostDetailPage() {
-	return <PostContent />;
+export default function PostDetailPage(props) {
+	return <PostContent post={props.post} />;
+}
+
+export async function getStaticProps(context) {
+	return {
+		props: {
+			post: getPostData((context.params.slug += '.md')),
+		},
+	};
+}
+
+export async function getStaticPaths() {
+	const postFileNames = getPostsFiles();
+
+	const slugs = postFileNames.map((fileName) =>
+		fileName.replace(/\.md$/, '')
+	);
+
+	return {
+		paths: slugs.map((slug) => ({
+			params: {
+				slug: slug,
+			},
+		})),
+		fallback: false,
+	};
 }
