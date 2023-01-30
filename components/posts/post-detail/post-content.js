d203bedf9e40823fea67cc29ffedab3d258e0848
huan_rozb <ivanrozb4225@gmail.com>
2023-01-30 15:45:30 +0200

    added md usage

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,14 +1,65 @@
 import PostHeader from '@/components/posts/post-detail/post-header';
 import classes from './post-content.module.css';
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/legacy/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 export default function PostContent(props) {
-	const imagePath = `/images/posts/${props.post.slug}/${props.post.image}`;
+	const { post } = props;
+	const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+	const customRenderers = {
+		// img(image) {
+		// 	return (
+		// 		<Image
+		// 			src={`/images/posts/${post.slug}/${image.src}`}
+		// 			alt={image.alt}
+		// 			width={800}
+		// 			height={400}
+		// 		/>
+		// 	);
+		// },
+		p(paragraph) {
+			const { node } = paragraph;
+
+			if (node.children[0].tagName === 'img') {
+				const image = node.children[0];
+
+				return (
+					<div className={classes.image}>
+						<Image
+							src={`/images/posts/${post.slug}/${image.properties.src}`}
+							alt={image.properties.alt}
+							width={800}
+							height={400}
+							layout="responsive"
+						/>
+					</div>
+				);
+			}
+			return <p>{paragraph.children}</p>;
+		},
+		code(code) {
+			const { className, children } = code;
+			const language = className.split('-')[1];
+
+			return (
+				<SyntaxHighlighter
+					style={atomDark}
+					language={language}
+					children={children}
+				/>
+			);
+		},
+	};
 
 	return (
 		<article className={classes.content}>
-			<PostHeader title={props.post.title} image={imagePath} />
-			<ReactMarkdown>{props.post.content}</ReactMarkdown>
+			<PostHeader title={post.title} image={imagePath} />
+			<ReactMarkdown components={customRenderers}>
+				{post.content}
+			</ReactMarkdown>
 		</article>
 	);
 }
